c23c8ce5926e5ac57e9cf8c1784e6650a3e3c834
Rebeca Murillo <rebeca.murillo.pro@gmail.com>
2020-09-02 22:34:44 +0200

    Add login page

---
+++ src/views/Login.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="about">
+    <form>
+      <label for="email">Email:</label><br>
+      <input id="email" required="required" placeholder="Correo electronico" type="text" name="email">
+      <br><br>
+      <label for="password">Contrasena</label>
+      <br>
+      <input id="email" required="required" placeholder="Contrasena" name="password" type="password">
+    </form>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class HelloWorld extends Vue {
+  @Prop() private msg!: string;
+}
+</script>
